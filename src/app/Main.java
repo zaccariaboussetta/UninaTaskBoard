e2f9140cd0c4808c50f3369ef8d7dd7819b06d12
Zaccaria-ctrlZ <boussettazaccaria@gmail.com>
2026-06-11 19:25:51 +0200

    Costruzione della GUI per l'applicativo.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,12 @@
 package app;
+import gui.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		new Window();
+		
 	}
 
 }
---
+++ src/gui/Window.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class Window extends JFrame{
+	
+	public Window() {
+		this.setVisible(true);
+	}
+	
+}
