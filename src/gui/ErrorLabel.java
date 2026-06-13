6bc1de2bedad559da7742a29087abf3495dcd203
Zack <boussettazaccaria@gmail.com>
2026-06-13 15:57:06 +0200

    Creazione di label personalizzata per il feedback di eventuali errori che possono avvenire nell'interfaccia grafica.

---
+++ src/gui/ErrorLabel.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.awt.Color;
+
+import javax.swing.JLabel;
+
+public class ErrorLabel extends JLabel{
+	
+	public ErrorLabel() {
+		super("");
+		this.setForeground(Color.RED);
+	}
+	
+}
