14f8c1872b0dfeb74bdc942d0919dd1f714b115f
Zack <boussettazaccaria@gmail.com>
2026-06-14 21:22:44 +0200

    Modifica del pannello welcome.

--- src/gui/WelcomePanel.java
+++ src/gui/WelcomePanel.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -13,16 +14,24 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
+import controllers.ProgettoController;
 import controllers.SessionController;
+import dao.ProgettoDAO;
+import dao.postgres.ProgettoDAOPostgres;
+import entities.Progetto;
 import entities.Utente;
 
 public class WelcomePanel extends JPanel implements ActionListener{
 	
 	private WindowApp windowApplication;
-	private JButton showButton;
-	private JButton createProjectButton;
+	private JButton openButton;
+	private JButton createButton;
+	DefaultTableModel modelloTabellaProgetti;
 	
 	public WelcomePanel(WindowApp windowApp) {
 		this.windowApplication = windowApp;
@@ -38,40 +47,73 @@ public class WelcomePanel extends JPanel implements ActionListener{
 		
 		String nomeUtente = SessionController.getInstance().getUtenteLoggato().getNome(); 
 		
-		JLabel welcomeLabel = new JLabel("Welcome to Unina Task Board " + nomeUtente);
+		JLabel welcomeLabel = new JLabel("Welcome to Unina Task Board, " + nomeUtente + " !");
 		welcomeLabel.setFont(new Font("SansSerif", Font.BOLD, 24));
 		welcomeLabel.setAlignmentX(CENTER_ALIGNMENT);
 		
+		
+		
+		String[] colonne = {"nome", "descrizione", "data creazione", "data consegna"};
+		modelloTabellaProgetti = new DefaultTableModel(colonne, 0);
+		JTable tabellaProgetti = new JTable(modelloTabellaProgetti);
+		
+		this.loadProjects();
+
+		
+		JScrollPane scrollProjectsPanel = new JScrollPane(tabellaProgetti);
+		scrollProjectsPanel.setPreferredSize(new Dimension(500, 400));
+		scrollProjectsPanel.setBorder(BorderFactory.createEmptyBorder());
+		
+		
+		openButton = new StyledButton("Apri");
+		createButton = new StyledButton("Crea");;
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setBackground(Color.WHITE);
 		buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.X_AXIS));
-		buttonsPanel.setAlignmentX(CENTER_ALIGNMENT);
-		
-		showButton = new StyledButton("Mostra progetti");
-		
-		createProjectButton = new StyledButton("Crea progetto");
-		
-		buttonsPanel.add(showButton);
-		buttonsPanel.add(Box.createRigidArea(new Dimension(50,0)));
-		buttonsPanel.add(createProjectButton);
+		buttonsPanel.add(openButton);
+		buttonsPanel.add(Box.createRigidArea(new Dimension(15, 0)));
+		buttonsPanel.add(createButton);
 		
 		
-		innerWelcomePanel.add(Box.createRigidArea(new Dimension(0, 100)));
+		innerWelcomePanel.add(Box.createRigidArea(new Dimension(0, 25)));
 		innerWelcomePanel.add(welcomeLabel);
-		innerWelcomePanel.add(Box.createRigidArea(new Dimension(0, 100)));
+		innerWelcomePanel.add(Box.createRigidArea(new Dimension(0, 25)));
+		innerWelcomePanel.add(scrollProjectsPanel);
+		innerWelcomePanel.add(Box.createRigidArea(new Dimension(0, 10)));
 		innerWelcomePanel.add(buttonsPanel);
+		innerWelcomePanel.add(Box.createRigidArea(new Dimension(0, 10)));
 		
 		this.add(innerWelcomePanel);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == showButton) {
+		if(e.getSource() == openButton) {
 			
 		}
 		
-		if(e.getSource() == createProjectButton) {
+		if(e.getSource() == createButton) {
 			
+		}
+		
+	}
+	
+	public void loadProjects() {
+		
+		ProgettoDAO progettoDAO = new ProgettoDAOPostgres();
+		ProgettoController progettoController = new ProgettoController(progettoDAO);
+		
+		ArrayList<Progetto> listProgetto = progettoController.getProgettiUtente();
+		
+		for(Progetto prog : listProgetto) {
+			
+			String[] row = {
+					prog.getNome(),
+					prog.getDescrizione(),
+					prog.getDataCreazione().toString(),
+					prog.getDataConsegna().toString()
+			};
+			
+			modelloTabellaProgetti.addRow(row);
 		}
 		
 	}
