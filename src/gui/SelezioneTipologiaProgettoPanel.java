5da3cf15843e297bd50e3174d6cb1135539c5266
Zaccaria-ctrlZ <boussettazaccaria@gmail.com>
2026-06-15 15:10:42 +0200

    Sviluppo del funzionamento del pannello selezione progetto.

--- src/gui/SelezioneTipologiaProgettoPanel.java
+++ src/gui/SelezioneTipologiaProgettoPanel.java
@@ -2,15 +2,31 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
-public class SelezioneTipologiaProgettoPanel extends JPanel{
+public class SelezioneTipologiaProgettoPanel extends JPanel implements ActionListener{
 	
-	public SelezioneTipologiaProgettoPanel(Dimension dimension) {
+	private CreateProjectFrame parentFrame;
+	private JButton avantiButton;
+	private JButton indietroButton;
+	private JButton annullaButton;
+	private JCheckBox checkSviluppo;
+	private JCheckBox checkGenerico;
+	private JCheckBox checkEsame;
+	
+	
+	public SelezioneTipologiaProgettoPanel(Dimension dimension, CreateProjectFrame parentFrame) {
+		
+		this.parentFrame = parentFrame;
 		this.setPreferredSize(dimension);
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
@@ -26,16 +42,26 @@ public class SelezioneTipologiaProgettoPanel extends JPanel{
 		JLabel titoloLabel = new JLabel("Selezionare tipologia di progetto");
 		titoloLabel.setAlignmentX(LEFT_ALIGNMENT);
 		
-		JCheckBox checkGenerico = new JCheckBox("Progetto - generico");
+		checkGenerico = new JCheckBox("Progetto - generico");
 		checkGenerico.setAlignmentX(LEFT_ALIGNMENT);
 		
-		JCheckBox checkSviluppo = new JCheckBox("Progetto - sviluppo applicativo");
+		checkSviluppo = new JCheckBox("Progetto - sviluppo applicativo");
 		checkSviluppo.setAlignmentX(LEFT_ALIGNMENT);
 		
-		JCheckBox checkEsame = new JCheckBox("Progetto - preparazione esame");
+		checkEsame = new JCheckBox("Progetto - preparazione esame");
 		checkEsame.setAlignmentX(LEFT_ALIGNMENT);
 		
+		//Bottoni
+		JPanel buttonsPanel = new JPanel();
 		
+		avantiButton = new JButton("Avanti >");
+		avantiButton.addActionListener(this);
+		
+		annullaButton = new JButton("Annulla");
+		annullaButton.addActionListener(this);
+	
+		buttonsPanel.add(annullaButton);
+		buttonsPanel.add(avantiButton);
 		
 		rightPanel1.add(titoloLabel);
 		rightPanel1.add(checkGenerico);
@@ -44,6 +70,54 @@ public class SelezioneTipologiaProgettoPanel extends JPanel{
 		
 		this.add(leftPanel1);
 		this.add(rightPanel1);
+		this.add(buttonsPanel);
+	}
+
+	@Override
+public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == avantiButton) {
+			
+			if(checkSviluppo.isSelected() && checkEsame.isSelected()) {
+				
+				parentFrame.showPanel("SVILUPPO ESAME");
+				
+			}
+			else if(checkSviluppo.isSelected()) {
+				
+				parentFrame.showPanel("SVILUPPO");
+				
+			}
+			else if(checkEsame.isSelected()) {
+				
+				parentFrame.showPanel("ESAME");
+				
+			}
+			else if(checkGenerico.isSelected()) {
+				
+				parentFrame.showPanel("GENERICO");
+				
+			}
+			else {
+				//TODO: Gestire mancata selezione.
+			}
+			
+		}
+		
+		if(e.getSource() == indietroButton) {
+			
+			avantiButton.setEnabled(true);
+			indietroButton.setEnabled(false);
+			
+		}
+		
+		
+		if(e.getSource() == annullaButton) {
+			
+			parentFrame.dispose();
+			
+		}
+		
 	}
 
 }
