5613c0f241e6e5103a497b1c05b5853151b84858
zaccariaboussetta <boussettazaccaria@gmail.com>
2026-06-26 11:17:25 +0200

    Implementazione dei DAO per Postgres

---
+++ src/dao/postgres/MembroDAOPostgres.java
@@ -0,0 +1,17 @@
+package dao.postgres;
+
+import java.util.ArrayList;
+
+import dao.MembroDAO;
+import entities.Membro;
+import entities.Progetto;
+
+public class MembroDAOPostgres implements MembroDAO {
+
+	@Override
+	public ArrayList<Membro> getMembriByProgetto(Progetto progetto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/dao/postgres/TaskDAOPostgres.java
@@ -0,0 +1,17 @@
+package dao.postgres;
+
+import java.util.ArrayList;
+
+import dao.TaskDAO;
+import entities.Attivita;
+import entities.Progetto;
+
+public class TaskDAOPostgres implements TaskDAO {
+
+	@Override
+	public ArrayList<Attivita> getTasksByProgetto(Progetto progetto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
