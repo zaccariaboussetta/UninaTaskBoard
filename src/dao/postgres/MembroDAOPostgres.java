768e16bceb1402008c1f60095fae4228ab896efc
Carlo De Gregorio <carlodegregorio05@gmail.com>
2026-07-01 20:53:35 +0200

    implementati MembroDAOPostgres e TaskDAOPostgres

--- src/dao/postgres/MembroDAOPostgres.java
+++ src/dao/postgres/MembroDAOPostgres.java
@@ -1,17 +1,57 @@
 package dao.postgres;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import dao.MembroDAO;
 import entities.Membro;
 import entities.Progetto;
+import entities.Utente;
 
 public class MembroDAOPostgres implements MembroDAO {
 
 	@Override
 	public ArrayList<Membro> getMembriByProgetto(Progetto progetto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Membro> listMembri = new ArrayList<>();
+		
+		String query = "SELECT m.ruolo, m.data_adesione, m.stato_partecipazione, m.ore_lavoro, "
+				     + "u.matricola, u.nome, u.cognome "
+				     + "FROM membro m JOIN utente u ON m.matricola = u.matricola "
+				     + "WHERE m.id_progetto = ?;";
+		
+		try (Connection connectionToDatabase = DatabaseConnection.getInstance();
+		     PreparedStatement pstmt = connectionToDatabase.prepareStatement(query)) {
+			
+			pstmt.setInt(1, progetto.getIdProgetto());
+			
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while(rs.next()) {
+					Utente utente = new Utente(
+							rs.getString("nome"),
+							rs.getString("cognome"),
+							rs.getString("matricola")
+					);
+					
+					Membro membro = new Membro(
+							rs.getString("ruolo"),
+							rs.getDate("data_adesione").toLocalDate(),
+							rs.getString("stato_partecipazione"),
+							rs.getFloat("ore_lavoro"),
+							progetto,
+							utente
+					);
+					
+					listMembri.add(membro);
+				}
+			}
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return listMembri;
 	}
-
 }
--- src/dao/postgres/TaskDAOPostgres.java
+++ src/dao/postgres/TaskDAOPostgres.java
@@ -1,17 +1,88 @@
 package dao.postgres;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import dao.TaskDAO;
 import entities.Attivita;
+import entities.AttivitaDocumentazione;
+import entities.AttivitaSviluppo;
 import entities.Progetto;
 
 public class TaskDAOPostgres implements TaskDAO {
 
 	@Override
 	public ArrayList<Attivita> getTasksByProgetto(Progetto progetto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ArrayList<Attivita> listAttivita = new ArrayList<>();
+		
+		String queryDocu = "SELECT id_attivita_docu, descrizione, data_creazione, scadenza, "
+				         + "stato_avanzamento, titolo_docu, formato, sezione, link_risorsa "
+				         + "FROM attivita_documentazione "
+				         + "WHERE id_progetto = ?;";
+		
+		String querySviluppo = "SELECT id_attivita_sviluppo, descrizione, data_creazione, scadenza, "
+				             + "stato_avanzamento, tipologia_sviluppo, linguaggio_programmazione, nome_branch "
+				             + "FROM attivita_sviluppo "
+				             + "WHERE id_progetto = ?;";
+		
+		try (Connection connectionToDatabase = DatabaseConnection.getInstance()) {
+			
+			try (PreparedStatement pstmtDocu = connectionToDatabase.prepareStatement(queryDocu)) {
+				pstmtDocu.setInt(1, progetto.getIdProgetto());
+				
+				try (ResultSet rsDocu = pstmtDocu.executeQuery()) {
+					while(rsDocu.next()) {
+						java.sql.Date sqlScadenza = rsDocu.getDate("scadenza");
+						java.time.LocalDate scadenza = (sqlScadenza != null) ? sqlScadenza.toLocalDate() : null;
+						
+						AttivitaDocumentazione attDocu = new AttivitaDocumentazione(
+								rsDocu.getInt("id_attivita_docu"),
+								rsDocu.getString("descrizione"),
+								rsDocu.getDate("data_creazione").toLocalDate(),
+								scadenza,
+								rsDocu.getString("stato_avanzamento"),
+								rsDocu.getString("titolo_docu"),
+								rsDocu.getString("formato"),
+								rsDocu.getString("sezione"),
+								rsDocu.getString("link_risorsa"),
+								progetto
+						);
+						listAttivita.add(attDocu);
+					}
+				}
+			}
+			
+			try (PreparedStatement pstmtSvilu = connectionToDatabase.prepareStatement(querySviluppo)) {
+				pstmtSvilu.setInt(1, progetto.getIdProgetto());
+				
+				try (ResultSet rsSvilu = pstmtSvilu.executeQuery()) {
+					while(rsSvilu.next()) {
+						java.sql.Date sqlScadenza = rsSvilu.getDate("scadenza");
+						java.time.LocalDate scadenza = (sqlScadenza != null) ? sqlScadenza.toLocalDate() : null;
+						
+						AttivitaSviluppo attSvilu = new AttivitaSviluppo(
+								rsSvilu.getInt("id_attivita_sviluppo"),
+								rsSvilu.getString("descrizione"),
+								rsSvilu.getDate("data_creazione").toLocalDate(),
+								scadenza,
+								rsSvilu.getString("stato_avanzamento"),
+								rsSvilu.getString("tipologia_sviluppo"),
+								rsSvilu.getString("linguaggio_programmazione"),
+								rsSvilu.getString("nome_branch"),
+								progetto
+						);
+						listAttivita.add(attSvilu);
+					}
+				}
+			}
+			
+		} catch(Exception exe) {
+			exe.printStackTrace();
+		}
+		
+		return listAttivita;
 	}
-
 }
